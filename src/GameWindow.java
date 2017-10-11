5148b50e8dbb7259a8610a1dbff478e106e73f7c
NguyenKhacDuy <duythachson@gmail.com>
2017-10-11 20:48:48 +0700

    Added player, back ground drawing Added size fixing, window

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class GameWindow extends JFrame{
 
@@ -7,11 +9,48 @@ public class GameWindow extends JFrame{
 
     public GameWindow(){
         this.setSize(800,600);
-        this.setVisible(true);
 
         this.canvas = new GameCanvas();
         this.setContentPane(this.canvas);
+        this.addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent windowEvent) {
+
+            }
+
+            @Override
+            public void windowClosing(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+
+            @Override
+            public void windowClosed(WindowEvent windowEvent) {
+
+            }
+
+            @Override
+            public void windowIconified(WindowEvent windowEvent) {
+
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent windowEvent) {
+
+            }
+
+            @Override
+            public void windowActivated(WindowEvent windowEvent) {
+
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent windowEvent) {
+
+            }
+        });
+        this.setResizable(false);
+
+        this.canvas.setVisible(true);
+        this.setVisible(true);
     }
-
-
 }
